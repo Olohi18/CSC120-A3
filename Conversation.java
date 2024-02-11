6be140f4cb7750d5b860987d53623d671f8f441b
Olohi John <ojohn@smith.edu>
2024-02-11 01:14:08 -0500

    Define da few methods,

--- Conversation.java
+++ Conversation.java
@@ -1,19 +1,49 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.Random;
+import java.util.Arrays;
 class Conversation {
 
   public static void main(String[] arguments) {
 
   Scanner sc = new Scanner(System.in);
+  Random rand = new Random();
 
-    System.out.println("Welcome to my guessing game");
-    int rounds = sc.nextInt();
+  //Initializes random responses
+  String resp[] = {"That sounds fun", "Tell me more", "Can you elaborate on that?", "How old are you?", "What?", "Pardon?"};
+  List <String> response = Arrays.asList(resp);
+  int length = response.size();
 
-    System.out.println("Hi there, what do you want to talk about?");
+  System.out.println(length);  
+  int x = response.get()
 
-    
+  // System.out.println(response);
+  //int index = response.get(rand.nextInt(response.charAt()));
 
+  //Initializes game
+  System.out.println("Enter number of rounds please");
+  int rounds = sc.nextInt();
+  sc.nextLine();
 
+  //
 
+  System.out.println("Hi there, what do you want to talk about?");
+  for (int i = 1; i <= rounds; i++){
+    String input = sc.nextLine();
+    System.out.println("");
+    }
+  System.out.println("Goodbye. Thanks for talking to me");
+  sc.close();
 
   }
+
 }
+
+  
+
+  
+
+
+    int x = response.get(rand.nextInt(length));
+
+    int randomElement = givenList.get(rand.nextInt(givenList.size()));
